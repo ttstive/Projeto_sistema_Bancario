9cfe53e9111371053cc4de5e89ebd4aa357b4108
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-12-03 16:41:17 -0300

    Update func_main.c
    Organizada e definida a função de trasnferência, agora os valores podem ser trasnferidos por meio do numero da conta;

--- func_main.c
+++ func_main.c
@@ -7,6 +7,7 @@ int main() {
     int opcoes;
     usuarios user[MAX_USUARIOS];
     int indice;
+    int totalUsers = 3;
 
     do {
         printf("Ola, bem vindo ao nosso sistema bancário.\n");
@@ -30,7 +31,7 @@ int main() {
                 }
                 break;
 
-            case 2: 
+            case 2:
                 printf("Digite o índice do usuário que você deseja depositar: ");
                 scanf("%d", &indice);
                 if (indice >= 0 && indice < MAX_USUARIOS) {
@@ -39,7 +40,7 @@ int main() {
                     printf("Índice de usuário inválido.\n");
                 }
                 break;
-
+                
             case 3:
                 printf("Digite o índice do usuário que você deseja sacar");
                 scanf("%d", &indice);
@@ -50,33 +51,20 @@ int main() {
                     printf("índice de usuário inválido.\n");
                 }
                 break;
+                
 
             case 4:
-                printf("digite o índice da conta rementente");
+                 printf("Digite o índice da conta remetente: ");
+                int indice;
                 scanf("%d", &indice);
-                if (indice<0 || indice >MAX_USUARIOS)
-                {
-                    printf("indíce usuário invalido\n");                    
-                }
-                else if (indice>=0 && indice <MAX_USUARIOS)
-                {
-                    Tranferencia1(&user[indice], indice);
-
-                }
-                               
-                break;
-            
-            case 6:
-                printf("Digite o índice do usuário cuja conta você deseja remover: ");
-                scanf("%d", &indice);
-                getchar(); // Limpa o buffer de entrada
-                if (indice >= 0 && indice < MAX_USUARIOS) {
-                    removeConta(user, indice);
+                if (indice < 0 || indice >= totalUsers) {
+                    printf("Índice de usuário inválido\n");
                 } else {
-                    printf("Índice de usuário inválido.\n");
+                    Tranferencia1(&user[indice], user, totalUsers);
                 }
                 break;
-            case 7:
+
+           case 7:
                 printf("Digite o índice do titular da conta que você deseja editar: ");
                 scanf("%d", &indice);
                 getchar(); // Limpa o buffer de entrada
