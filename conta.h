0c0de6ea8b152600bfbfa1f9e0f58e59ef0a29f9
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-12-06 10:23:51 -0300

    Update conta.h
    Atualização da criação de conta com validador de cpf, gerador de numero conta, e contas sendo criada de forma unica, mas com uma permição de até 2 contas disponíveis para criação.
    Atualização da função de extrato, chamando agora pelo numero da conta do usuário.

--- conta.h
+++ conta.h
@@ -4,20 +4,72 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_USUARIOS 3
+#define MAX_USUARIOS 2
 
 typedef struct {
     char nome[30];
-    long long cpf;
+    char cpf[15]; // Alterado para array de char
     int idade;
     float saldo;
     long long numeroConta;
 } usuarios;
 
-long long gerarNumeroConta(long long cpf, int idade) {
-    return cpf + idade - 17; // Adjusted formula for generating the account number
-}
+int validaCPF(const char* cpf) {
+    int i, j, digito1 = 0, digito2 = 0;
+    int cpf_array[11];
 
+    // Verifica se o formato do CPF está correto
+    for (i = 0, j = 0; i < 14; i++) {
+        if ((i == 3 || i == 7) && cpf[i] == '.') {
+            continue;
+        } else if (i == 11 && cpf[i] == '-') {
+            continue;
+        } else if (cpf[i] >= '0' && cpf[i] <= '9') {
+            cpf_array[j] = cpf[i] - '0';
+            j++;
+        } else {
+            return 1;
+        }
+    }
+
+    // Calcula o primeiro dígito verificador
+    for (i = 0, j = 10; i < 9; i++, j--)
+        digito1 += cpf_array[i] * j;
+    digito1 %= 11;
+    if (digito1 < 2)
+        digito1 = 0;
+    else
+        digito1 = 11 - digito1;
+
+    // Calcula o segundo dígito verificador
+    for (i = 0, j = 11; i < 10; i++, j--)
+        digito2 += cpf_array[i] * j;
+    digito2 %= 11;
+    if (digito2 < 2)
+        digito2 = 0;
+    else
+        digito2 = 11 - digito2;
+
+    // Verifica se os dígitos verificadores são iguais aos informados
+    if (cpf_array[9] == digito1 && cpf_array[10] == digito2)
+        return 0;
+    else
+        return 1;
+} 
+
+long long gerarNumeroConta(char* cpf, int idade) {
+    char cpf_sem_pontos[12]; // Armazena o CPF sem pontos ou traço
+    int j = 0;
+    for (int i = 0; i < 15; i++) {
+        if (cpf[i] != '.' && cpf[i] != '-') {
+            cpf_sem_pontos[j] = cpf[i];
+            j++;
+        }
+    }
+    cpf_sem_pontos[j] = '\0'; // Finaliza a string
+    long long cpf_as_number = atoll(cpf_sem_pontos); // Converte o CPF para long long
+    return cpf_as_number + idade - 17; // Fórmula ajustada para gerar o número da conta
+}
 
 void criaConta(usuarios* user) {
     FILE *fp = fopen("contas.txt", "a");
@@ -39,23 +91,26 @@ void criaConta(usuarios* user) {
     }
 
     printf("Digite seu cpf: ");
-    scanf("%lld", &(user->cpf));
-    user->saldo = 0;
+    scanf("%s", user->cpf);
+
+    // Verifica se o CPF tem o formato correto
+    if (strlen(user->cpf) != 14 || user->cpf[3] != '.' || user->cpf[7] != '.' || user->cpf[11] != '-') {
+        printf("Formato de CPF inválido. Insira o CPF no formato correto (000.000.000-00).\n");
+        fclose(fp);
+        return;
+    }
 
     user->numeroConta = gerarNumeroConta(user->cpf, user->idade);
     printf("Sua conta foi criada, guarde seu número.\n");
     printf("O número da conta gerado é: %lld\n", user->numeroConta);
 
-    fprintf(fp, "Nome: %s, Idade: %d, CPF: %lld, Numero da Conta: %lld, Saldo: %.2f\n", user->nome, user->idade, user->cpf, user->numeroConta, user->saldo);
+    fprintf(fp, "Nome: %s, Idade: %d, CPF: %s, Numero da Conta: %lld, Saldo: %.2f\n", user->nome, user->idade, user->cpf, user->numeroConta, user->saldo); // Alterado %lld para %s para cpf
     fclose(fp);
 }
-// deposita o valor
 
 void depositaValor(usuarios* user, usuarios* allUsers, int totalUsers) {
     float deposito;
-
     long long num_conta_deposita;
-
 
     printf("Informe o número da conta: ");
     scanf("%lld", &num_conta_deposita);
@@ -80,8 +135,7 @@ void depositaValor(usuarios* user, usuarios* allUsers, int totalUsers) {
     allUsers[deposito_index].saldo += deposito;
 
     printf("Valor depositado foi de: R$ %.2f\n", deposito);
-    printf("Seu saldo atual é: R$ %.2f\n", user->saldo);
-
+    printf("Seu saldo atual é: R$ %.2f\n", allUsers[deposito_index].saldo);
 
     FILE *fp = fopen("contas.txt", "a");
     if (fp == NULL) {
@@ -89,11 +143,16 @@ void depositaValor(usuarios* user, usuarios* allUsers, int totalUsers) {
         return;
     }
 
-    fprintf(fp, "Nome :%s , Depósito: %.2f, Saldo: %.2f, Conta: %lld\n", user->nome, deposito, user->saldo, user->numeroConta);
+    fprintf(fp, "Nome :%s , Depósito: %.2f, Saldo: %.2f, Conta: %lld\n", allUsers[deposito_index].nome, deposito, allUsers[deposito_index].saldo, allUsers[deposito_index].numeroConta);
     fclose(fp);
 }
 
 void sacaValor(usuarios* user, usuarios* allUsers, int totalUsers){
+    if (user == NULL) {
+        printf("Usuário inválido.\n");
+        return;
+    }
+
     float saque;
     long long num_conta_saque;
 
@@ -117,7 +176,7 @@ void sacaValor(usuarios* user, usuarios* allUsers, int totalUsers){
     printf("Insira um valor que deseja sacar: ");
     scanf("%f", &saque);
 
-    if (user->saldo < saque) {
+    if (allUsers[saque_index].saldo < saque) {
         printf("Seu saldo não é suficiente para realizar essa transferência.\n");
         return;
     }
@@ -125,16 +184,15 @@ void sacaValor(usuarios* user, usuarios* allUsers, int totalUsers){
     allUsers[saque_index].saldo -= saque;
 
     printf("Valor sacado foi de : R$ %.2f\n", saque);
-    printf("Seu saldo atual é: R$ %.2f\n", user->saldo);
+    printf("Seu saldo atual é: R$ %.2f\n", allUsers[saque_index].saldo);
 
-    
     FILE *fp = fopen("contas.txt", "a");
     if (fp == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
 
-    fprintf(fp, "Nome :%s , Saque: %.2f, Saldo: %.2f, Conta: %lld \n", user->nome, saque, user->saldo, user->numeroConta);
+    fprintf(fp, "Nome :%s , Saque: %.2f, Saldo: %.2f, Conta: %lld \n", allUsers[saque_index].nome, saque, allUsers[saque_index].saldo, allUsers[saque_index].numeroConta);
     fclose(fp);
 }
 
@@ -165,8 +223,12 @@ void editarInformacoes(usuarios* user, usuarios* allUsers, int totalUsers) {
     printf("Digite a nova idade: ");
     scanf("%d", &(allUsers[edita_index].idade));
 
+    char new_cpf[15];
     printf("Digite o novo CPF: ");
-    scanf("%lld", &(allUsers[edita_index].cpf));
+    scanf("%s", new_cpf);
+
+    // Se o CPF for válido, atualiza o CPF
+    strcpy(allUsers[edita_index].cpf, new_cpf);
 
     printf("Informações do titular da conta atualizadas com sucesso!\n");
 
@@ -196,8 +258,15 @@ void editarInformacoes(usuarios* user, usuarios* allUsers, int totalUsers) {
 
     for (int j = 0; j < MAX_USUARIOS; j++) {
         if (j == edita_index) {
-            fprintf(fp, "Nome: %s, Idade: %d, CPF: %lld, Numero da Conta: %lld, Saldo: %.2f\n", allUsers[edita_index].nome, allUsers[edita_index].idade, allUsers[edita_index].cpf, allUsers[edita_index].numeroConta, allUsers[edita_index].saldo);
+            fprintf(fp, "Nome: %s, Idade: %d, CPF: %s, Numero da Conta: %lld, Saldo: %.2f\n", allUsers[edita_index].nome, allUsers[edita_index].idade, allUsers[edita_index].cpf, allUsers[edita_index].numeroConta, allUsers[edita_index].saldo);
         } else {
+            // Check if the line ends with a newline character
+            int len = strlen(linhas[j]);
+            if (linhas[j][len - 1] != '\n') {
+                // If not, add a newline character
+                linhas[j][len] = '\n';
+                linhas[j][len + 1] = '\0';
+            }
             fprintf(fp, "%s", linhas[j]);
         }
     }
@@ -209,11 +278,15 @@ void editarInformacoes(usuarios* user, usuarios* allUsers, int totalUsers) {
     free(linhas);
 }
 
-void Transferencia1(usuarios* user, usuarios* allUsers, int totalUsers) {
+void Transferencia1(usuarios* user, usuarios* allUsers, int totalUsers){
+    if (user == NULL) {
+        printf("Usuário inválido.\n");
+        return;
+    }
+
     float transfere;
     long long num_conta_destino;
     long long num_conta_remetente;
-
 
     printf("Informe o número da conta: ");
     scanf("%lld", &num_conta_remetente);
@@ -232,11 +305,10 @@ void Transferencia1(usuarios* user, usuarios* allUsers, int totalUsers) {
         return;
     }
 
-
     printf("Informe o valor a transferir: ");
     scanf("%f", &transfere);
 
-    if (user->saldo < transfere) {
+    if (allUsers[remetente_index].saldo < transfere) {
         printf("Seu saldo não é suficiente para realizar essa transferência.\n");
         return;
     }
@@ -263,14 +335,13 @@ void Transferencia1(usuarios* user, usuarios* allUsers, int totalUsers) {
 
     printf("Transferência de R$ %.2f realizada com sucesso para %s.\n", transfere, allUsers[destinatario_index].nome);
 
-    // Save transaction details to file
     FILE *fp = fopen("contas.txt", "a");
     if (fp == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
 
-    fprintf(fp, "Nome: %s, Transferência: %.2f, Conta Origem: %lld, Conta Destino: %lld\n", user->nome, transfere, num_conta_remetente, num_conta_destino);
+    fprintf(fp, "Nome: %s, Transferência: %.2f, Conta Origem: %lld, Conta Destino: %lld\n", allUsers[remetente_index].nome, transfere, num_conta_remetente, num_conta_destino);
     fclose(fp);
 }
 
@@ -299,8 +370,8 @@ void removeConta(usuarios* user, int indice) {
     }
 
     for (int j = 0; j < MAX_USUARIOS; j++) {
-        if (j != indice) {
-            fprintf(fp, "Nome: %s, Idade: %d, CPF: %lld, Numero da Conta: %lld, Saldo: %.2f\n", user[j].nome, user[j].idade, user[j].cpf, user[j].numeroConta, user[j].saldo);
+        if (j != indice && strcmp(user[j].nome, "") != 0) {
+            fprintf(fp, "Nome: %s, Idade: %d, CPF: %s, Numero da Conta: %lld, Saldo: %.2f\n", user[j].nome, user[j].idade, user[j].cpf, user[j].numeroConta, user[j].saldo);
         }
     }
     fclose(fp);
@@ -316,11 +387,27 @@ void removeConta(usuarios* user, int indice) {
     printf("Conta removida com sucesso!\n");
 }
 
-void extrato(usuarios* user){
-    printf("Número da Conta: %lld\n", user->numeroConta);
-    printf("Nome do Titular: %s\n", user->nome);
-    printf("Idade: %d\n", user->idade);
-    printf("Cpf: %lld\n", user->cpf);
-    printf("Saldo Atual: R$ %.2f\n", user->saldo);
-    return;
+void extrato(usuarios* allUsers, int totalUsers) {
+    int user_index = -1;
+    long long int num_conta_extrato;
+    printf("informe o numero da conta: ");
+    scanf("%lld", &num_conta_extrato);
+    for (int i = 0; i < totalUsers; i++) {
+        if (allUsers[i].numeroConta == num_conta_extrato) {
+            user_index = i;
+            break;
+        }
+    }
+
+    if (user_index == -1) {
+        printf("Conta não encontrada.\n");
+        return;
+    } else {
+        usuarios* user = &allUsers[user_index];
+        printf("Número da Conta: %lld\n", user->numeroConta);
+        printf("Nome do Titular: %s\n", user->nome);
+        printf("Idade: %d\n", user->idade);
+        printf("Cpf: %s\n", user->cpf);
+        printf("Saldo Atual: R$ %.2f\n", user->saldo);
+    }
 }
